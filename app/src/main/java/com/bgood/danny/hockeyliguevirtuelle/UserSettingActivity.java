1707bcf5ff2dd5e1fdd4a1f3178857949248bec7
dannybeaulieu <danny.beaulieu.pq@gmail.com>
2014-11-19 09:25:17 -0500

    debug null on selected value

--- app/src/main/java/com/bgood/danny/hockeyliguevirtuelle/UserSettingActivity.java
+++ app/src/main/java/com/bgood/danny/hockeyliguevirtuelle/UserSettingActivity.java
@@ -4,6 +4,10 @@ import android.os.*;
 
 public class UserSettingActivity extends PreferenceActivity
 {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
    public static class Prefs1Fragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
         @Override
         public void onCreate(Bundle savedInstanceState) {
